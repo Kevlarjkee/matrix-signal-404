9c2f750279e4b8c6e768efe4732179397d29da46
Alexander <kvdz.d1z@gmail.com>
2018-07-01 21:20:08 +0300

    Add files via upload
    init

---
+++ 404.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+<link href="./error_pages/matrix.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+<div id='error-page'>
+  <div id='error-inner'>
+    <div class='box-404'><a href='http://192.168.1.150'>404</a></div>
+    <h1>PAGE NOT FOUND</h1>
+    <p>OOPS! You make the wrong turn, let us lead you to the right way,</p>
+    <p> return to 
+      <a href='http://192.168.1.150' title='HOMEPAGE'>homepage</a> ?
+    </p>
+    
+    </div>
+  </div>
+</div>
+<canvas id="c"></canvas>
+<script src='./error_pages/matrix.js' type='text/javascript'/></script>
+<!-- 
+<script src='https://cdn.rawgit.com/Brando07/share/newbe/matrix-rain-seocips.js' type='text/javascript'/></script>
+-->
+ </body>
+ </html>
