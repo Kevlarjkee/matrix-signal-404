41c99edaa5032790c36dd608fe8cd5e10da7f3a9
Alexander <kvdz.d1z@gmail.com>
2018-07-02 04:38:41 +0300

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -6,14 +6,16 @@ c.height = window.innerHeight;
 c.width = window.innerWidth;
 
 //chinese characters - taken from the unicode charset
+var binary = "01";
 var chinese = "田由甲申甴电甶男甸甹町画甼甽甾甿畀畁畂畃畄畅畆畇畈畉畊畋界畍畎畏畐畑";
 var eng = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
 var rus = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХШЩЪЫЬЭЮЯ";
 var jap = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわゐゑを";
 
 //converting the string into an array of single characters
+binary = binary.split("");
 // chinese = chinese.split("");
-eng = eng.split("");
+// eng = eng.split("");
 // rus = rus.split("");
 // jap = jap.split("");
 
@@ -40,10 +42,11 @@ function draw()
 	for(var i = 0; i < drops.length; i++)
 	{
 		//a random lang character to print
+		var text = binary[Math.floor(Math.random()*binary.length)];
 //		var text = jap[Math.floor(Math.random()*jap.length)];
 //		var text = rus[Math.floor(Math.random()*rus.length)];
 //		var text = chinese[Math.floor(Math.random()*chinese.length)];
-		var text = eng[Math.floor(Math.random()*eng.length)];
+//		var text = eng[Math.floor(Math.random()*eng.length)];
 		//x = i*font_size, y = value of drops[i]*font_size
 		ctx.fillText(text, i*font_size, drops[i]*font_size);
 		
