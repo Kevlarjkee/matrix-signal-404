5bda2378a83b4891bd1add167b789b9cea4d3b50
Alexander <kvdz.d1z@gmail.com>
2018-07-08 17:18:22 +0500

    Update 404.php
    lit changes

--- 404.php
+++ 404.php
@@ -1,21 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+<title>Error 404</title>
 <head>
 <link href="matrix.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 <div id='error-page'>
   <div id='error-inner'>
-    <div class='box-404'><a href='#'>404</a></div>
+    <div class='box-404'><a href='/'>404</a></div>
     <h1>PAGE NOT FOUND</h1>
     <p>OOPS! You make the wrong turn, let us lead you to the right way,</p>
     <p> return to 
-      <a href='#' title='HOMEPAGE'>homepage</a> ?
+      <a href='/' title='HOMEPAGE'>homepage</a> ?
     </p>
     </div>
   </div>
 </div>
-<canvas id="c"></canvas>
+<canvas class="rotate" id="c"></canvas>
 <script src='matrix.js' type='text/javascript'/></script>
-</body>
-</html>
+ </body>
+ </html>
